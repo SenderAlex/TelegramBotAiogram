4a41979a73bdc7900cf4e53a8f3007b7114af448
Alexander Sender <alexander.sender@gmail.com>
2025-04-17 15:59:02 +0300

    установил aiogram

---
+++ main.py
@@ -0,0 +1,11 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+
+bot = Bot('')
+dp = Dispatcher()
+
+async def main():
+    await dp.start_polling()
+
+if __name__ == '__main__':
+    asyncio.run(main())
